e8435779f8c619683459ae6248552d05f544cf85
sewonkimm <swon962@gmail.com>
2021-01-27 16:30:16 +0900

    feat: 🍎 Main 페이지 markup
    - google material icon 추가
    - spoqa han sans 웹폰트 추가
    - colors.scss파일에 프로젝트에서 사용하는 모든 색상 변수화
    - 반응형 미디어쿼리설정: mobile first
    - Main 페이지 레이아웃 구조 설계
    - Header component markup
    
    ✅ Closes: #S04P12A202-83

---
+++ frontend/src/components/main/BadgeBoard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="badgeBoard">
+    <Board title="나의 뱃지함" />
+  </div>
+</template>
+<script>
+import Board from "./Board.vue";
+
+export default {
+  name: "BadgeBoard",
+  components: {
+    Board,
+  },
+};
+</script>
