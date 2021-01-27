3c0d383749b1ee89fb51e6da9c3c257522ba5f75
김세원 <swon962@gmail.com>
2021-01-27 02:02:43 +0000

    #S04P12A202-84 SCSS 적용

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
         singleQuote: false,
         semi: true,
         useTabs: false,
+        tabWidth: 2,
         trailingComma: 'es5',
         printWidth: 100,
         bracketSpacing: false,
