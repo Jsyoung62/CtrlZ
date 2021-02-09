ecf94048096cae8ccce55186463f523c2aca28e0
sewonkimm <swon962@gmail.com>
2021-02-10 01:55:49 +0900

    feat: 🍎 에러페이지 추가
    ✅ Closes: #S04P13A202-60

---
+++ frontend/src/views/404NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="error">
+    <p class="message">
+      404 Page not found!
+    </p>
+    <Logo />
+    Ctrl Z your page
+  </div>
+</template>
+<script>
+import { Logo } from "@/assets/index.js";
+import "@/components/css/error.scss";
+
+export default {
+  name: "NotFound",
+  components: {
+    Logo,
+  },
+};
+</script>
