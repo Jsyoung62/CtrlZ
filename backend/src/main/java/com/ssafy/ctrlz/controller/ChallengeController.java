56188957021f2b1e640930720d763b9b7bcfe2fc
Sueyoung Jeong <sue900462@gmail.com>
2021-02-14 22:46:25 +0900

    feat: 🍎 사용자별 추천 챌린지 기능 구현
    - 사용자별 추천 챌린지 기능 구현
    
    - 3가지 경우로 나누어서 추천 챌린지 challengeList 설정
    1. userId가 없는 경우
    2. userId가 있는 경우 : zbtiId가 있는 경우
    3. userId가 있는 경우 : zbtiId가 없는 경우
    
    - 참여자 수 participants가 많은 순서대로 challengeList 정렬 (내림차순)
    
    - 사용자가 이미 참여한 챌린지는 제외하는 부분 구현
    
    - 사용자에게 데일리 챌린지와 기업 챌린지를 제외하고 기본적으로 2개의 챌린지를 추천하지만, 추천 챌린지가 2개 미만일 경우 2개 미만만 추천
    
    ✅ Closes: #S04P13A202-74

--- backend/src/main/java/com/ssafy/ctrlz/controller/ChallengeController.java
+++ backend/src/main/java/com/ssafy/ctrlz/controller/ChallengeController.java
@@ -2,6 +2,7 @@ package com.ssafy.ctrlz.controller;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.ssafy.ctrlz.model.Challenge;
+import com.ssafy.ctrlz.model.ChallengeStatus;
 import com.ssafy.ctrlz.service.ChallengeService;
 import com.ssafy.ctrlz.service.ChallengeStatusService;
+import com.ssafy.ctrlz.service.UserScoreService;
+import com.ssafy.ctrlz.service.ZbtiResultService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -29,8 +33,14 @@ public class ChallengeController {
 	@Autowired
 	private ChallengeStatusService challengeStatusService;
 
+	@Autowired
+	private UserScoreService userScoreService;
+
+	@Autowired
+	private ZbtiResultService zbtiResultService;
+
 	@ApiOperation(value = "전체 챌린지 데이터")
-	@GetMapping(value="/all")
+	@GetMapping(value = "/all")
 	public ResponseEntity<List<Challenge>> findAll() {
 		List<Challenge> challengeList = challengeService.findAll();
 		String challengeId;
@@ -44,7 +54,7 @@ public class ChallengeController {
 	}
 
 	@ApiOperation(value = "특정 챌린지 데이터")
-	@GetMapping(value="/")
+	@GetMapping(value = "/")
 	public Challenge findByChallenge(@RequestParam String challengeId) {
 		Challenge challenge = challengeService.findByChallenge(challengeId);
 		challenge.setParticipants(challengeStatusService.countInprogressByChallenge(challenge.getChallengeId()));
@@ -53,7 +63,7 @@ public class ChallengeController {
 	}
 
 	@ApiOperation(value = "난이도별 챌린지 데이터")
-	@GetMapping(value="/level")
+	@GetMapping(value = "/level")
 	public ResponseEntity<List<Challenge>> findByLevel(@RequestParam String levelId) {
 		List<Challenge> challengeList = challengeService.findByLevel(levelId);
 		String challengeId;
@@ -69,7 +79,7 @@ public class ChallengeController {
 	}
 
 	@ApiOperation(value = "타입별 챌린지 데이터")
-	@GetMapping(value="/type")
+	@GetMapping(value = "/type")
 	public ResponseEntity<List<Challenge>> findByChallengeType(@RequestParam String challengeType) {
 		List<Challenge> challengeList = challengeService.findByChallengeType(challengeType);
 		String challengeId;
@@ -85,7 +95,7 @@ public class ChallengeController {
 	}
 
 	@ApiOperation(value = "진행중인 챌린지 데이터")
-	@GetMapping(value="/inprogress")
+	@GetMapping(value = "/inprogress")
 	public ResponseEntity<List<Challenge>> findByChallengeInProgress() {
 		String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 		List<Challenge> challengeList = challengeService.findByChallengeInProgress(now);
@@ -102,7 +112,7 @@ public class ChallengeController {
 	}
 
 	@ApiOperation(value = "종료된 챌린지 데이터")
-	@GetMapping(value="/closed")
+	@GetMapping(value = "/closed")
 	public ResponseEntity<List<Challenge>> findByChallengeClosed() {
 		String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 		List<Challenge> challengeList = challengeService.findByChallengeClosed(now);
@@ -115,6 +125,57 @@ public class ChallengeController {
 			}
 		}
 
+		return new ResponseEntity<List<Challenge>>(challengeList, HttpStatus.OK);
+	}
+
+	@ApiOperation(value = "사용자별 추천 챌린지 데이터")
+	@GetMapping(value = "/recommend")
+	public ResponseEntity<List<Challenge>> findByRecommendChallenge(@RequestParam String userId) {
+		String zbtiId;
+		String challengeId;
+		List<Challenge> challengeList;
+		List<ChallengeStatus> userChallengeList = null;
+		
+		if (userId.equals("")) {
+			challengeList = challengeService.findByChallengeType("일상");
+		}
+		else {
+			zbtiId = userScoreService.findByUser(userId).getZbtiId();
+			userChallengeList = challengeStatusService.findByUser(userId);
+			if (zbtiId != null) {
+				String zbtiType = zbtiResultService.findByZbti(zbtiId).getZbtiType();
+				challengeList = challengeService.findByChallengeType(zbtiType);
+			}
+			else {
+				challengeList = challengeService.findByChallengeType("일상");
+			}
+		}
+
+		for (int i = 0; i < challengeList.size(); i++) {
+			challengeId = challengeList.get(i).getChallengeId();
+			challengeList.get(i).setParticipants(challengeStatusService.countInprogressByChallenge(challengeId));
+		}
+
+		challengeList.sort(new Comparator<Challenge>() {
+			@Override
+			public int compare(Challenge c1, Challenge c2) {
+				return Long.compare(c2.getParticipants(), c1.getParticipants());
+			}
+		});
+
+		if (userChallengeList != null) {
+			for (int i = 0; i < challengeList.size(); i++) {
+				for (int j = 0; j < userChallengeList.size(); j++) {
+					if (challengeList.get(i).getChallengeId().equals(userChallengeList.get(j).getChallenge().getChallengeId())) {
+						challengeList.remove(i);
+					}
+				}
+			}
+		}
+
+		if (challengeList.size() >= 2) {
+			return new ResponseEntity<List<Challenge>>(challengeList.subList(0, 2), HttpStatus.OK);
+		}
 		return new ResponseEntity<List<Challenge>>(challengeList, HttpStatus.OK);
 	}
 
